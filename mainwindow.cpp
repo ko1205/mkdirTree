01f4c9094b69b7215cbcf268ce144e05e97d38be
Gilhan Choi <ghchoi.choi@gmail.com>
2017-04-18 19:50:34 +0900

    file menu add

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,15 @@
 #include "mainwindow.h"
+#include <QMenuBar>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    QMenu *fileMenu;
+    QAction *newAction = new QAction(tr("&New project"),this);
+    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu -> addAction(newAction);
+
+    menuBar()->setNativeMenuBar(0);
 }
 
 MainWindow::~MainWindow()
