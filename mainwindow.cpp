13a7d3a687ed916b7311b41bab21ba042490edff
Gilhan Choi <ghchoi.choi@gmail.com>
2017-04-19 14:59:34 +0900

    base interface

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,12 @@
 #include "mainwindow.h"
 #include <QMenuBar>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QTreeView>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,7 +19,7 @@ MainWindow::~MainWindow()
 
 }
 
-int MainWindow::setWindow()
+void MainWindow::setWindow()
 {
     QMenu *fileMenu;
     QAction *newAction = new QAction(tr("&New project"),this);
@@ -20,5 +27,40 @@ int MainWindow::setWindow()
     fileMenu -> addAction(newAction);
 
     menuBar()->setNativeMenuBar(0);
-    return 0;
+
+    QWidget *centerWidget = new QWidget();
+    QVBoxLayout *baseLayout = new QVBoxLayout();
+    QHBoxLayout *topLineLayout = new QHBoxLayout();
+    QLabel *rootPathLabel = new QLabel(tr("rootPath"));
+    QLineEdit *rootPathEdit = new QLineEdit();
+    QPushButton *rootPathButton = new QPushButton(tr("..."));
+
+    QHBoxLayout *viewLayout = new QHBoxLayout();
+    QHBoxLayout *buttonLayout = new QHBoxLayout();
+
+
+    QTreeView *templateView = new QTreeView();
+    QTreeView *preView = new QTreeView();
+
+    QPushButton *makeTreeButton = new QPushButton(tr("Make"));
+    QPushButton *cancelButton = new QPushButton(tr("cancel"));
+
+    topLineLayout ->addWidget(rootPathLabel);
+    topLineLayout ->addWidget(rootPathEdit);
+    topLineLayout ->addWidget(rootPathButton);
+
+    viewLayout->addWidget(templateView);
+    viewLayout->addWidget(preView);
+
+    buttonLayout->addWidget(makeTreeButton);
+    buttonLayout->addWidget(cancelButton);
+
+    baseLayout->addLayout(topLineLayout);
+    baseLayout->addLayout(viewLayout);
+    baseLayout->addLayout(buttonLayout);
+
+    centerWidget->setLayout(baseLayout);
+    setCentralWidget(centerWidget);
+
+//    return 0;
 }
